e12389a0a3e9cfe9a2f3406c36191dd755657f90
sourada <sourada@strycek.ms.mff.cuni.cz>
2026-03-08 21:35:04 +0100

    implement dummy sorting of distractors

---
+++ src/distractor_sorting.py
@@ -0,0 +1,15 @@
+
+
+
+def dummy_shuffle(distractors: list, row: dict) -> list:
+    """
+    A dummy sorting method that simply shuffles the distractors randomly. This is used as a baseline to compare against
+    more sophisticated sorting methods. It does not take into account the content of the distractors or the question.
+    """
+
+    # create a copy of the list to avoid modifying the original
+    import random
+
+    distractors = distractors.copy()
+    random.shuffle(distractors)
+    return distractors
