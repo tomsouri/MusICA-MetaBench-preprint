df16f7483ac409579759e0f1728a90e19c052b08
Tomas Sourada <sourada@ufal.mff.cuni.cz>
2026-03-16 15:12:51 +0100

    add sample running script

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J run      # name of job
+#SBATCH -p cpu-ms       # name of partition or queue (default=cpu-troja)
+#SBATCH -o run.out  # name of output file for this submission script
+#SBATCH -e run.err  # name of error file for this submission script
+
+# to submit the job, you need to be ssh-ed at one of the lrc or sol machines. (from geri/freki/blackbird, ssh lrc1 or sol1)
+# Then use:
+# sbatch -p cpu-ms -c2 --mem=4G run.sh
+
+.venv/bin/python3 generate_benchmark.py --config benchmark-generation-config.yaml \
+    --benchmark_file "benchmark_v1.tsv" \
+    --submodalities "audio.mastermix.wav" "symbolic.musicxml" "visual.pdf" \
+    --questions_per_subcategory_count 5 \
+    --seed 42
+
+.venv/bin/python3 run_benchmark.py --config benchmark-run-config.yaml \
+    --models "google/gemini-2.0-flash-lite-001" \
+    --url "https://openrouter.ai/api/v1/chat/completions" \
+    --api-key-env "OPENROUTER_API_KEY" \
+    --benchmark_file "benchmark_v1.tsv" \
+    --sheet_name "experiment-branch-01" \
+    --modalities "audio" "symbolic" "visual" \
+    --run_id "experiment-branch-01-run-01" \
+    --text_only_baseline
+
