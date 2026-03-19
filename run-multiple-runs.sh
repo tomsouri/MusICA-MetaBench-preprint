f1008e6384e01441209d8c1224e658cd76ba549f
Tomas Sourada <sourada@ufal.mff.cuni.cz>
2026-03-19 04:35:31 +0100

    update the run script to automatically compute the mean and stddev after running everything

--- run-multiple-runs.sh
+++ run-multiple-runs.sh
@@ -5,8 +5,8 @@
 #SBATCH -e run.err  # name of error file for this submission script
 
 # to submit the job, you need to be ssh-ed at one of the lrc or sol machines. (from geri/freki/blackbird, ssh lrc1 or sol1)
-# Then use:
-# sbatch -p cpu-ms -c2 --mem=4G run_multiple.sh
+# Then use: sbatch --dependency=afterany:<JOB_ID>
+# sbatch -p cpu-ms -c2 --mem=4G --dependency=afterany:6358156 run_multiple.sh
 
 # - "google/gemini-3.1-pro-preview"
 # - "google/gemini-3.1-flash-lite-preview"
@@ -26,7 +26,9 @@ models=(
 
 
 qpersubcategory=15
+# seeds=({42..43})
 seeds=({42..52})
+
 
 benchmarks=() # Initialize an empty array
 
@@ -53,6 +55,13 @@ done
 for model in "${models[@]}"; do
     echo "Running the benchmarks for model ${model}..."
 
+    # 1. Replace all forward slashes / with underscores
+    safe_model="${model//\//_}"
+
+    # 2. Replace spaces with underscores
+    safe_model="${safe_model// /_}"
+
+
     resfiles=()
     textonlyresfiles=()
 
@@ -62,7 +71,7 @@ for model in "${models[@]}"; do
 
         echo "Running on a benchmark with random seed: $seed"
     
-        resfile="${seed}_${qpersubcategory}_{model}.res.tsv"
+        resfile="${seed}_${qpersubcategory}_${safe_model}.res.tsv"
         resfiles+=("$resfile")
     
 
@@ -72,15 +81,16 @@ for model in "${models[@]}"; do
             --api-key-env "OPENROUTER_API_KEY" \
             --benchmark_file "$benchmark_file" \
             --modalities "audio" "symbolic" "visual" \
-            --run_id "rs${seed}"  \
+            --run_id "Nrs${seed}"  \
             --max_waiting_time_per_request 300 \
-            --generate_new_list_with_logs \
             --verbose \
-            --evaluation_output_file "${resfile}"
+            --evaluation_output_file "${resfile}" \
+            --dry_run \
+            --generate_new_list_with_logs \
 
         echo "================================================================================"
 
-        toresfile="${seed}_${qpersubcategory}_to_{model}.res.tsv"
+        toresfile="${seed}_${qpersubcategory}_to_${safe_model}.res.tsv"
         textonlyresfiles+=("$toresfile")
 
         run .venv/bin/python3 run_benchmark.py --config eval-config.yaml \
@@ -89,17 +99,18 @@ for model in "${models[@]}"; do
             --api-key-env "OPENROUTER_API_KEY" \
             --benchmark_file "$benchmark_file" \
             --modalities "audio" "symbolic" "visual" \
-            --run_id "to_rs${seed}"  \
+            --run_id "Nto_rs${seed}"  \
             --max_waiting_time_per_request 300 \
-            --generate_new_list_with_logs \
             --text_only_baseline \
             --verbose \
-            --evaluation_output_file "${toresfile}"
+            --evaluation_output_file "${toresfile}" \
+            --dry_run \
+            --generate_new_list_with_logs \
 
         echo "================================================================================"
     done
 
-    dir="averaged/${model}/${qpersubcategory}"
+    dir="averaged/${safe_model}/${qpersubcategory}"
     mkdir -p $dir
 
     .venv/bin/python3 compute_mean_stddev.py --list_of_tsvs "${resfiles[@]}" --output_file "${dir}/res.tsv"
