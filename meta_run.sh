817a9cd52bf038e1be40849869f193253577eb6f
Tomas Sourada <sourada@ufal.mff.cuni.cz>
2026-04-07 12:54:09 +0200

    remove unused running scripts

--- meta_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -J run      # name of job
-#SBATCH -p cpu-ms       # name of partition or queue (default=cpu-troja)
-#SBATCH -o run.out  # name of output file for this submission script
-#SBATCH -e run.err  # name of error file for this submission script
-
-run bash run-multiple-runs.sh
