d2a8505cdc85829602b887ba5ff044d02433a601
kackav <katia.vendrame@seznam.cz>
2026-04-11 01:20:06 +0200

    corrected dominant generation in question of get_harmonic_tonality

--- src/ground_truth_and_distractor_pool_extractions.py
+++ src/ground_truth_and_distractor_pool_extractions.py
@@ -1100,7 +1100,8 @@ class AnswerDistractorExtractors:
         else:
 
             keys_dict["tonic"]  = key_signatures.name
-            keys_dict["dominant"]  = key.Key(key_signatures.getDominant().name, key_signatures.mode).asKey().name
+            #keys_dict["dominant"]  = key.Key(key_signatures.getDominant().name, key_signatures.mode).asKey().name
+            keys_dict["dominant"]  = key_signatures.getDominant().name
             keys_dict["relative minor/major"] = key_signatures.relative.name                    
             target_key_signature_name = self.dict_tonality_ontology[keys_dict[harmonic_function]]            
             if not(self.random_distractors):
